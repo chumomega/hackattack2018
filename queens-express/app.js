e802c1396371d5024ff1fb7265105936be78ef5c
Jean <jeansebastien.surena@baruchmail.cuny.edu>
2018-10-27 19:48:18 +0000

    inital directory adds

---
+++ queens-express/app.js
@@ -0,0 +1,25 @@
+const express         = require('express'),
+      app             = express(),
+      ejs             = require('ejs'),
+      methodOverride  = require('method-override'),
+      bodyParser      = require('body-parser'),
+      passport        = require('passport');
+      
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+
+app.use(require("express-session")({
+    secret: "Secret message",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("Queens Mentor has started."); 
+});
