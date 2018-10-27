ce46de6b4e191b6c8b54bde63d2c80d323f00fc8
Jean <jeansebastien.surena@baruchmail.cuny.edu>
2018-10-27 21:37:49 +0000

    update app.js to use index.js routes and have optional port of 8000

--- queens-express/app.js
+++ queens-express/app.js
@@ -5,6 +5,9 @@ const express         = require('express'),
       bodyParser      = require('body-parser'),
       passport        = require('passport');
       
+let port = process.env.PORT || 8000,
+    indexRoutes = require('./routes/index');
+      
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -16,10 +19,8 @@ app.use(require("express-session")({
     saveUninitialized: false
 }));
 
-app.get('/', function(req, res){
-    res.render('home');
-});
+app.use(indexRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("Queens Mentor has started."); 
+app.listen(port, process.env.IP, function(){
+   console.log('Queens Mentor has started.'); 
 });
